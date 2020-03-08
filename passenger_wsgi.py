77769bded820f48f3580eed350f6e91fc5ca28cf
quadrismegistus <ryan.heuser@gmail.com>
2020-03-08 09:03:14 +0000

    reverting

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,24 +1,10 @@
-# import os
-# import sys
-#
-#
-# sys.path.insert(0, os.path.dirname(__file__))
-#
-#
-# def application(environ, start_response):
-#     start_response('200 OK', [('Content-Type', 'text/plain')])
-#     message = 'It works!\n'
-#     version = 'Python %s\n' % sys.version.split()[0]
-#     response = '\n'.join([message, version])
-#     return [response.encode()]
+import os
+import sys
+import importlib.util
 
-# import os
-# import sys
-# import importlib.util
-#
-# sys.path.insert(0, os.path.dirname(os.path.realpath('__file__')))
-#
-# spec = importlib.util.spec_from_file_location("wsgi", "__init__.py")
-# wsgi = importlib.util.module_from_spec(spec)
-# spec.loader.exec_module(wsgi)
-# application = wsgi.app
+sys.path.insert(0, os.path.dirname(os.path.realpath('__file__')))
+
+spec = importlib.util.spec_from_file_location("wsgi", "__init__.py")
+wsgi = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(wsgi)
+application = wsgi.app
