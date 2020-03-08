2c5a74524092a72a0ee9d5b14c449b26bc2958dd
quadrismegistus <ryan.heuser@gmail.com>
2020-03-08 08:36:26 +0000

    init

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import os
+
+from flask import Flask, request, render_template, redirect, url_for
+
+project_root = os.path.dirname(os.path.realpath('__file__'))
+template_path = os.path.join(project_root, 'app/templates')
+static_path = os.path.join(project_root, 'app/static')
+app = Flask(__name__, template_folder=template_path, static_folder=static_path)
+
+@app.route('/')
+def index():
+    return 'Hello, World'
