e1c0a6cbb17018bcce52bd30f96e945b67de0b7f
quadrismegistus <ryan.heuser@gmail.com>
2020-03-08 08:42:51 +0000

    init

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -12,13 +12,13 @@
 #     response = '\n'.join([message, version])
 #     return [response.encode()]
 
-import os
-import sys
-import importlib.util
-
-sys.path.insert(0, os.path.dirname(os.path.realpath('__file__')))
-
-spec = importlib.util.spec_from_file_location("wsgi", "__init__.py")
-wsgi = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(wsgi)
-application = wsgi.app
+# import os
+# import sys
+# import importlib.util
+#
+# sys.path.insert(0, os.path.dirname(os.path.realpath('__file__')))
+#
+# spec = importlib.util.spec_from_file_location("wsgi", "__init__.py")
+# wsgi = importlib.util.module_from_spec(spec)
+# spec.loader.exec_module(wsgi)
+# application = wsgi.app
