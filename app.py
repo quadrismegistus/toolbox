f6d04aa7388fae99d782bad36350438b03144304
quadrismegistus <ryan.heuser@gmail.com>
2020-03-08 21:56:16 +0000

    updates

--- app.py
+++ app.py
@@ -8,14 +8,11 @@ static_path = os.path.join(project_root, 'static')
 app = Flask(__name__, template_folder=template_path, static_folder=static_path)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-
 @app.route('/word')
 @app.route('/word/<word>')
 def analyze_word(word=None):
-    return render_template('word.html')
+    return template_path
+    #return render_template('word.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
